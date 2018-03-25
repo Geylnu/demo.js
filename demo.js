b2bed2ee5aaf4217a241a38422471c0df6fe90f1
yanglei <yanglei1997630@gmail.com>
2018-03-25 13:33:10 +0800

    首次上传

---
+++ demo.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+var fs = require('fs')
+var dirName =process.argv[2]
+
+fs.mkdirSync("./"+dirName)
+process.chdir("./"+dirName)
+fs.mkdirSync('css')
+fs.mkdirSync('js')
+
+fs.writeFileSync("./index.html","<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>\n")
+fs.writeFileSync("css/style.css","h1{clor red;}\n")
+fs.writeFileSync("./js/main.js","var string =\"Hello World\"\nalert(string)\n")
